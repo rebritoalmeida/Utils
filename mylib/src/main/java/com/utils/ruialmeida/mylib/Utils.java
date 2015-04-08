43071c0967738323480adbb0680f64cf8e692a4e
Rui Almeida <rui.emanuel@gmail.com>
2015-04-08 15:44:30 +0100

    First Commit

---
+++ mylib/src/main/java/com/utils/ruialmeida/mylib/Utils.java
@@ -0,0 +1,17 @@
+package com.utils.ruialmeida.mylib;
+
+/**
+ * Created by ruialmeida on 4/8/15.
+ */
+public abstract class Utils {
+
+    /**
+     * Math Utils
+     *
+     * @author rui
+     *
+     */
+    public static class math extends com.utils.ruialmeida.mylib.math {
+    }
+
+}
---
+++ mylib/src/main/java/com/utils/ruialmeida/mylib/math.java
@@ -0,0 +1,18 @@
+package com.utils.ruialmeida.mylib;
+
+/**
+ * Created by ruialmeida on 4/8/15.
+ */
+public class math{
+
+    /**
+     * Sums two numbers
+     *
+     * @param numberOne The first number to be sum.
+     * @param numberTwo The second number to be sum.
+     */
+    public static int sum(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+}
